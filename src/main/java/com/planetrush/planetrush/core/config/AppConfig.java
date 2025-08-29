f3ab1e6d1c94f1c6cea04bac2f33549720d19252
simhani1 <simhani1@gmail.com>
2025-08-29 16:19:20 +0900

    fix: 캐시 config 클래스 분리

--- src/main/java/com/planetrush/planetrush/core/config/AppConfig.java
+++ src/main/java/com/planetrush/planetrush/core/config/AppConfig.java
@@ -1,16 +1,10 @@
 package com.planetrush.planetrush.core.config;
 
-import java.util.concurrent.TimeUnit;
-
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestTemplate;
-
-import com.github.benmanes.caffeine.cache.Caffeine;
 
 @EnableAsync
 @EnableRetry
@@ -22,12 +16,4 @@ public class AppConfig {
 		return new RestTemplate();
 	}
 
-	@Bean
-	public CacheManager cacheManager() {
-		CaffeineCacheManager cacheManager = new CaffeineCacheManager("challengeProgressAvg");
-		cacheManager.setCaffeine(
-			Caffeine.newBuilder().expireAfterWrite(24, TimeUnit.HOURS).recordStats()
-		);
-		return cacheManager;
-	}
 }
---
+++ src/main/java/com/planetrush/planetrush/core/config/CacheConfig.java
@@ -0,0 +1,40 @@
+package com.planetrush.planetrush.core.config;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Configuration
+public class CacheConfig {
+
+	@Bean
+	public CacheManager cacheManager() {
+		CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+		CacheType[] values = CacheType.values();
+		for (CacheType ct : CacheType.values()) {
+			cacheManager.registerCustomCache(ct.getCacheName(),
+				Caffeine.newBuilder().expireAfterWrite(ct.getDuration(), ct.getTimeUnit()).build());
+		}
+		return cacheManager;
+	}
+
+	@Getter
+	@AllArgsConstructor
+	enum CacheType {
+		CHALLENGE_AVG("challenge-avg", 24, TimeUnit.HOURS),
+		REGISTER_RESIDENT_RESPONSE("register-resident-response", 10, TimeUnit.SECONDS),
+		DELETE_RESIDENT_RESPONSE("delete-resident-response", 10, TimeUnit.SECONDS);
+
+		final String cacheName;
+		final int duration;
+		final TimeUnit timeUnit;
+	}
+}
